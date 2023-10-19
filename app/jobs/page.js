a8769e8e684d92b1e039a19440d60f4d233df9a8
Dariohg <rubendarioh99@gmail.com>
2023-10-19 00:35:49 -0600

    half-home
    home 70%

--- app/jobs/page.js
+++ app/jobs/page.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import Img from "next/image";
+import ImgHome from "@/public/assets/busqueda_trabajo_asesoramiento.webp"
+import ImgVacant from "@/public/assets/istockphoto-1070456274-170667a.jpg"
+import "./home.css"
+import ButtonSearch from "@/components/ButtonSearch";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+      <div>
+          <div className="mensaje_home">
+            <p className="mensaje">Encuentra miles de vacantes para los mejores trabajos</p>
+            <Img src={ImgHome} alt="imgane-home" className="imagen_home"></Img>
+          </div>
+          <div>
+              <p className="mensaje_dos">Las mejores empresas están aquí</p>
+          </div>
+          <div className="contain_empresas">
+              <Img src={ImgVacant} alt="imgane-vacant" className="imagen_vacant"></Img>
+          </div>
+
+          <div>
+              <p className="mensaje_tres">¿Tienes una empresa?</p>
+              <p className="mensaje_tres">Publica tu vacanate con nosotros</p>
+              <div className="contain_cuadrito">
+                  <p className="mensaje_dos">Encuentra al mejor empleado para tu empresa</p>
+                  <div className="contain_button">
+                      <ButtonSearch/>
+                  </div>
+              </div>
+          </div>
+
+      </div>
   )
 }
 
