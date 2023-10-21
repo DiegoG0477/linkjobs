04ed60aca6e585a69aa96ebdd71742bc80637ffd
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-10-20 23:50:28 -0600

    job-card component was updated to reuse

--- app/jobs/vacants/page.js
+++ app/jobs/vacants/page.js
@@ -1,12 +1,22 @@
 import React from "react";
 import JobCard from "@/components/JobCard";
+import zumayaLogo from "@/public/assets/zumaya-img.png";
 
 export default function VacantsPage() {
   return (
     <>
       <h1 className="page-title">Mis Vacantes</h1>
 
-      <JobCard/>
+      <JobCard 
+        sinceDate="Hace 2 días"
+        vacant="Repartidor"
+        salary="14,000"
+        company="Zumaya"
+        location="Tuxtla Gutiérrez, Chiapas"
+        requests="9876"
+        image={zumayaLogo}
+        type="vacant"
+      />
       <JobCard/>
       <JobCard/>
     </>
--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -1,37 +1,62 @@
+"use client";
 import Img from "next/image";
-import zumayaLogo from "@/public/assets/zumaya-img.png";
+import { useEffect, useState } from "react";
 
 export default function JobCard(props) {
+  const [vacantType, setVacantType] = useState(false);
+  const [statusColor, setStatusColor] = useState("#083CAE");
+
+  useEffect(() => {
+    if (props.type === "vacant") {
+      setVacantType(true);
+    }
+
+    if (props.requestStatus === "Aceptado") {
+      setStatusColor("#4BC76D");
+    } else if (props.requestStatus === "Rechazado") {
+      setStatusColor("#C73737");
+    }
+  });
+
   return (
     <>
       <div className="job-card">
         <div>
-          <p className="job-date">Hace n días</p>
-          <h3 className="job-vacant">Puesto promocionado</h3>
-          <p>Salario</p>
+          <p className="job-date">{props.sinceDate}</p>
+          <h3 className="job-vacant">{props.vacant}</h3>
+          <p>{props.salary}</p>
 
-          <h3 style={{ marginTop: "7vh" }} className="job-vacant">
-            Empresa
-          </h3>
-          <p>Ubicación</p>
+          <h3 className="job-vacant">{props.company}</h3>
+          <p>{props.location}</p>
 
           <p style={{ marginTop: "5vh" }}>texto adicional complementario</p>
         </div>
 
         <div>
-          <p style={{ fontWeight: "bolder" }}>Solicitudes</p>
-          <div className="requests-number-box">
-            <p>12439</p>
-          </div>
+          {vacantType ? (
+            <>
+              <p style={{ fontWeight: "bolder" }}>Solicitudes</p>
+              <div className="requests-number-box">
+                <p>{props.requests}</p>
+              </div>
+            </>
+          ) : (
+            <>
+              <p style={{ fontWeight: "bolder" }}>STATUS</p>
+              <div
+                style={{ background: { statusColor } }}
+                className="request-status"
+              >
+                <p className="request-status-text">{props.requestStatus}</p>
+              </div>
+            </>
+          )}
 
           <div>
-            {" "}
-            <Img src={zumayaLogo} alt="" style={{ marginTop: "5vh" }} />
+            <Img src={props.image} className="company-logo" alt="" />
           </div>
 
-          <button style={{ marginTop: "8vh" }} className="show-requests-button">
-            Ver Postulados
-          </button>
+          <button className="show-requests-button">{vacantType ? (<p>Ver Postulados</p>) : (<p>Ver Detalles</p>)}</button>
         </div>
       </div>
     </>
